be068ba66559736795a438eb41f3796effa9338a
Yoanm <yoanm@users.noreply.github.com>
2022-08-28 13:10:10 +0200

    Fix version parsing

--- plugins/plugin_utils/ansible_utils.py
+++ plugins/plugin_utils/ansible_utils.py
@@ -4,6 +4,7 @@ from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
 
 from ansible import __version__ as ansible_version
+import re
 
 # Hack to avoid loading "typing" module at runtime (issue with sanity tests on python 2.7) while keeping MyPy happy
 MYPY = False
@@ -12,12 +13,20 @@ if MYPY:
 
 _version_split = ansible_version.split('.')
 
-VERSION_PART = (
-    int(_version_split.pop(0)),
-    int(_version_split.pop(0)),
-    int(_version_split.pop(0)),
-    [str(i) for i in _version_split]
-)  # type: Tuple[int, int, int, List[Text]]
+VERSION_PART = list()  # type: List[str]
+
+for part in _version_split:
+    try:
+        VERSION_PART.append(str(int(part)))
+    except ValueError:  # part may contain non-numeric chars (like 'rc1')
+        matches = re.match(r'^(\d+)(.*)\.$', part)
+        if matches is not None and matches.groups() is not None:
+            groups = list(matches.groups())  # type: List
+            if len(groups) > 1: # There is numeric chars at beginning
+                VERSION_PART.append(str(int(groups.pop(0))))
+            VERSION_PART.append(str(groups.pop(0)))
+        else:
+            VERSION_PART.append(str(part))
 
 SHORT_VERSION = '.'.join([str(VERSION_PART[0]), str(VERSION_PART[1])])
 SHORT_VERSION_FLOAT = float(SHORT_VERSION)
