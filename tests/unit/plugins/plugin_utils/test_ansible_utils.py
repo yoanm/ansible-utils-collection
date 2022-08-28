fbb65dca20d0c200ef071513a71b151100a587c6
Yoanm <yoanm@users.noreply.github.com>
2022-08-28 15:39:14 +0200

    Lint

--- tests/unit/plugins/plugin_utils/test_ansible_utils.py
+++ tests/unit/plugins/plugin_utils/test_ansible_utils.py
@@ -15,9 +15,9 @@ except ImportError:
         # Python 3,<3.4
         from imp import reload  # type: ignore
     except ImportError:
-        pass # < python 3, method globally defined
+        pass  # < python 3, method globally defined
 from ansible_collections.community.internal_test_tools.tests.unit.compat import unittest
-from ansible_collections.community.internal_test_tools.tests.unit.compat.mock import MagicMock, patch, PropertyMock
+from ansible_collections.community.internal_test_tools.tests.unit.compat.mock import patch
 from ansible_collections.yoanm.utils.plugins.plugin_utils import ansible_utils
 
 
@@ -25,14 +25,14 @@ class SimpleActionModule(unittest.TestCase):
 
     @patch('ansible.__version__', '1.2.3')
     def test_basic_version(self):
-        reload(ansible_utils) # Reload module else ansible.__version__ is the one defined during previous test
+        reload(ansible_utils)  # Reload module else ansible.__version__ is the one defined during previous test
         self.assertEqual(['1', '2', '3'], ansible_utils.VERSION_PART)
         self.assertEqual('1.2', ansible_utils.SHORT_VERSION)
         self.assertEqual(1.2, ansible_utils.SHORT_VERSION_FLOAT)
 
     @patch('ansible.__version__', '1.2rc1')
     def test_rc_version(self):
-        reload(ansible_utils) # Reload module else ansible.__version__ is the one defined during previous test
+        reload(ansible_utils)  # Reload module else ansible.__version__ is the one defined during previous test
         self.assertEqual(['1', '2', 'rc1'], ansible_utils.VERSION_PART)
         self.assertEqual('1.2', ansible_utils.SHORT_VERSION)
         self.assertEqual(1.2, ansible_utils.SHORT_VERSION_FLOAT)
